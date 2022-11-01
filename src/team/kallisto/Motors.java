5fb7b108c9935b5847585c1ff697f7991574f9a8
Johannes Schmatz <johannes_schmatz@gmx.de>
2022-11-01 19:12:53 +0100

    Motors now has some helper method, and only contains the motors that are used
    also they are now named by name and not by port

--- src/team/kallisto/Motors.java
+++ src/team/kallisto/Motors.java
@@ -3,35 +3,63 @@ package team.kallisto;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.Port;
 import lejos.robotics.RegulatedMotor;
 
 public class Motors {
 	public static void close() {
-		A.close();
-		B.close();
-		C.close();
-		D.close();
+		rotateTo0AndFltAndReset();
+
+		DRIVE.close();
+		MEASURE.close();
+		STEER.close();
 	}
 
-	private enum MotorType {
-		LARGE,
-		MEDIUM
+	public static void rotateTo0AndFltAndReset() {
+		MEASURE.rotateTo(0);
+		MEASURE.flt();
+
+		STEER.rotateTo(0);
+		STEER.flt();
+
+		reset();
 	}
 
-	private static RegulatedMotor motor(Port port, MotorType type) {
-		switch (type) {
-			case LARGE:
-				return new EV3LargeRegulatedMotor(port);
-			case MEDIUM:
-				return new EV3MediumRegulatedMotor(port);
-			default:
-				throw new RuntimeException("MotorType: " + type + " not found!");
+	public static void reset() {
+		DRIVE.resetTachoCount();
+		MEASURE.resetTachoCount();
+		STEER.resetTachoCount();
+	}
+
+	public static final LimitedScaledMotor DRIVE = new LimitedScaledMotor(new EV3LargeRegulatedMotor(MotorPort.D));
+	public static final LimitedScaledMotor MEASURE = new LimitedScaledMotor(new EV3MediumRegulatedMotor(MotorPort.B), 65, 6);
+	public static final RegulatedMotor STEER = new LimitedScaledMotor(new EV3LargeRegulatedMotor(MotorPort.A), 45, 15);
+
+	static {
+		reset();
+	}
+
+	public static void slowDrivingDown() {
+		// TODO: code, multiple calls should keep the speed the same
+
+	}
+
+	public static void speedDrivingUp() {
+		// TODO: code, multiple calls should keep the speed the same
+	}
+
+	public static int swingAroundTick() {
+		int currentAngle = MEASURE.getTachoCount();
+		if (MEASURE.hasReachedNegativeLimit(currentAngle)) {
+			MEASURE.rotateTo(MEASURE.positiveLimit, true);
+			return 1;
+		} else if (MEASURE.hasReachedPositiveLimit(currentAngle)) {
+			MEASURE.rotateTo(MEASURE.negativeLimit, true);
+			return -1;
 		}
+		return 0;
 	}
 
-	public static final RegulatedMotor A = motor(MotorPort.A, MotorType.MEDIUM);
-	public static final RegulatedMotor B = motor(MotorPort.B, MotorType.LARGE);
-	public static final RegulatedMotor C = motor(MotorPort.C, MotorType.LARGE);
-	public static final RegulatedMotor D = motor(MotorPort.D, MotorType.MEDIUM);
+	public static void startSwinging() {
+		MEASURE.rotateTo(MEASURE.positiveLimit, true);
+	}
 }
