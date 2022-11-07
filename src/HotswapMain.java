40b500b3fefff8cb172c7856d07d179367734fec
Johannes Schmatz <johannes_schmatz@gmx.de>
2022-11-07 16:31:19 +0100

    add the ability to hot swap classes to the ev3

---
+++ src/HotswapMain.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+public class HotswapMain {
+	public static void main(String[] args) {
+		//System.out.println("launching " + Arrays.toString(args));
+
+		String[] classPaths = {
+				"hotswap-agent.jar",
+				"/home/root/classes",
+				"/home/root/lejos/lib/ev3classes.jar",
+				"/home/root/lejos/lib/dbusjava.jar",
+				"/home/root/lejos/lib/opencv-2411.jar",
+				"/home/root/lejos/libjna/usr/share/java/jna.jar",
+				"."
+		};
+		String classPath = String.join(":", classPaths);
+
+		ProcessBuilder builder = new ProcessBuilder();
+		builder.command(
+				"/home/root/lejos/ejre-jdk-custom/bin/java",
+				"-classpath",
+				classPath,
+				"-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005",
+				"lejos.internal.ev3.EV3Wrapper",
+				"Main"
+		);
+		builder.inheritIO();
+		try {
+			builder.start().waitFor();
+		} catch (InterruptedException | IOException e) {
+			throw new RuntimeException(e);
+		}
+		//System.out.println("exiting");
+	}
+}
